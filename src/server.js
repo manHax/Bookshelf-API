b195a4ce2febe9f08d2eeaa25b26624cff146a5e
L Hakim <luqmanul.hakim@dwidasa.com>
2023-10-09 22:09:15 +0700

    init

--- src/server.js
+++ src/server.js
@@ -1,42 +1,10 @@
 const Hapi = require('@hapi/hapi');
 
-const http = require('http');
 const routes = require('./routes');
 
-function findAvailablePort(startingPort, callback) {
-  const server = http.createServer();
-
-  server.on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
-      findAvailablePort(startingPort + 1, callback);
-    } else {
-      callback(err);
-    }
-  });
-
-  server.on('listening', () => {
-    const { port } = server.address();
-    server.close(() => {
-      callback(null, port);
-    });
-  });
-
-  server.listen(startingPort, '0.0.0.0');
-}
-
 const init = async () => {
-  let availPort ;
-  // Contoh penggunaan:
-  findAvailablePort(9000, (err, port) => {
-    if (err) {
-      console.error('Error:', err);
-    } else {
-      console.log('Port yang tersedia:', port);
-      availPort = port;
-    }
-  });
   const server = Hapi.server({
-    port: availPort,
+    port: 5050,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
